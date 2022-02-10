9be2b0ac9c899b52338de53e0b89f6c8ce8759a9
Nojus R <42602537+had0pelagic@users.noreply.github.com>
2022-02-10 16:44:18 +0200

    Register page, navigation

--- 3deshop-frontend/src/App.js
+++ 3deshop-frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/RegisterPage";
+import Home from "./pages/HomePage";
 
-function App() {
+export default function App() {
+  // return <Register />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ 3deshop-frontend/src/pages/RegisterPage/index.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
+export default function Register() {
+  const [state, setState] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if (state.password === state.confirmPassword) {
+      console.log("registering");
+    } else {
+      console.log("error, passwords are not matching");
+    }
+  };
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  return (
+    <div className="flexContainer">
+      <TextField
+        required
+        id="username"
+        label="Username"
+        variant="standard"
+        margin="normal"
+        value={state.username}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        id="firstName"
+        label="First name"
+        variant="standard"
+        margin="normal"
+        value={state.firstName}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        id="lastName"
+        label="Last name"
+        variant="standard"
+        margin="normal"
+        value={state.lastName}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        id="email"
+        label="Email"
+        variant="standard"
+        margin="normal"
+        type="email"
+        value={state.email}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        id="password"
+        label="Password"
+        variant="standard"
+        margin="normal"
+        type="password"
+        value={state.password}
+        onChange={handleChange}
+      />
+      <TextField
+        required
+        id="confirmPassword"
+        label="Confirm password"
+        variant="standard"
+        margin="normal"
+        type="password"
+        value={state.confirmPassword}
+        onChange={handleChange}
+      />
+      <div className="mt40">
+        <Button variant="contained" onClick={handleSubmitClick}>
+          Register
+        </Button>
+      </div>
+    </div>
+  );
+}
